25d82773d89094a9f5334af7cbdd39bf1d3b7f50
Nabhitaaurellia <bitaaurelia5@gmail.com>
2024-09-27 10:56:42 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,13 @@
+nama_depan  = "Nabhita"
+nama_belakang = 'Aurellia'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '18'
+alamat = 'jl sungai selan gang masjid alhuda'
+kata_mutiara = "jika salah perbaiki,jika gagal cobalagi"
+
+print(nama_lengkap, '('+ usia + ')',',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
